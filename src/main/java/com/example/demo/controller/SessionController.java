4431dbb1d5d71190b85e3bce7614094a14a9b068
murata <murata.naoya.infratop@gmail.com>
2021-04-26 21:18:32 +0900

    sessionの作成

--- src/main/java/com/example/demo/controller/SessionController.java
+++ src/main/java/com/example/demo/controller/SessionController.java
@@ -1,6 +1,10 @@
 package com.example.demo.controller;
 
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,9 +13,9 @@ import com.example.demo.form.LoginForm;
 
 @Controller
 public class SessionController {
-	@RequestMapping(path = "/loginUsingForm", method = RequestMethod.GET)
-	public String login() {
-		return "session/loginUsingForm";
+	@RequestMapping(path = "/loginOnRequest", method = RequestMethod.GET)
+	public String loginOnRequest() {
+		return "session/loginOnRequest";
 	}
 	
 	@RequestMapping(path = "/doLogin", method = RequestMethod.GET)
@@ -20,9 +24,35 @@ public class SessionController {
 		return "session/login";
 	}
 	
-	@PostMapping(path="/doLoginUsingForm")
-	public String doLoginPost(LoginForm form) {
-		System.out.println("ユーザID：" + form.getUserId());
-		return "session/loginUsingForm";
+	@PostMapping(path="/doLoginOnRequest")
+	public String doLoginOnRequest(LoginForm form, Model model) {
+		model.addAttribute("userId", form.getUserId());
+		return "session/LoginOnRequest";
+	}
+	
+	@RequestMapping(path="/loginOnSession", method = RequestMethod.GET)
+	public String loginOnSession() {
+		return "session/loginOnSession";
+	}
+	
+	@PostMapping(path="/doLoginOnSession")
+	public String doLoginOnSession(LoginForm form, HttpSession session) {
+		if (form.getUserId() == 123) {
+			session.setAttribute("userId", form.getUserId());
+			return "redirect:/";
+		}else {
+			return "session/loginOnSession";
+		}
+	}
+	
+	@GetMapping("/logout")
+	public String logout(HttpSession session) {
+		session.invalidate();
+		return "redirect:/";
 	}
 }
+
+
+
+
+
