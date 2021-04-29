31996d65e2f28955ad5b70ffe19ae98e9529b6cb
murata <murata.naoya.infratop@gmail.com>
2021-04-29 15:47:00 +0900

    createできるところまで

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,12 +1,16 @@
 package com.example.demo.controller;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
+import com.example.demo.entity.Item;
+import com.example.demo.form.ItemForm;
 import com.example.demo.repository.ItemRepository;
 
 @Controller
@@ -59,6 +63,21 @@ public class ItemController {
 	public String ItemListSetDropdown(Model model) {
 		session.setAttribute("items", repository.findAll());
 		return "items/item_list_dropdown";
+	}
+	
+	@RequestMapping("/items/create/input")
+	public String createInput() {
+		return "items/create_input";
+	}
+	
+	@RequestMapping(path = "/items/create/complete", method = RequestMethod.POST)
+	public String createComplete(ItemForm form) {
+		Item item = new Item();
+		item.setName(form.getName());
+		item.setPrice(form.getPrice());
+		item.setId(form.getId());
+		repository.save(item);
+		return "redirect:/items/getOne/" + item.getId();
 	}
 }
 
---
+++ src/main/java/com/example/demo/form/ItemForm.java
@@ -0,0 +1,26 @@
+package com.example.demo.form;
+
+public class ItemForm {
+	private int id;
+	private String name;
+	private int price;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getPrice() {
+		return price;
+	}
+	public void setPrice(int price) {
+		this.price = price;
+	}
+	
+}
