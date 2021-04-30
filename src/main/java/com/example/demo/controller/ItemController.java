164b0a751dc7f55ad829ccdffde794f090aa42c1
murata <murata.naoya.infratop@gmail.com>
2021-05-01 08:26:04 +0900

    update処理完了

--- src/main/java/com/example/demo/controller/ItemController.java
+++ src/main/java/com/example/demo/controller/ItemController.java
@@ -1,13 +1,16 @@
 package com.example.demo.controller;
 
 import javax.servlet.http.HttpSession;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 import com.example.demo.entity.Item;
 import com.example.demo.repository.ItemRepository;
 
@@ -70,6 +73,19 @@ public class ItemController {
 	
 	@PostMapping("/items/create/complete")
 	public String createComplete(@ModelAttribute Item item) {
+		repository.save(item);
+		return "redirect:/items/getOne/" + item.getId();
+	}
+	
+	@GetMapping("/items/update/input/{id}")
+	public String updateInput(@PathVariable int id, Model model) {
+		model.addAttribute("item", repository.getOne(id));
+		return "items/update_input";
+	}
+	
+	@PostMapping("/items/update/complete/{id}")
+	public String updateComplete(@PathVariable int id, @ModelAttribute Item item) {
+		item.setId(id);
 		repository.save(item);
 		return "redirect:/items/getOne/" + item.getId();
 	}
